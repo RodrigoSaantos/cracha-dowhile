937c448eccdc5e5c9ecb11f60fad0d757f7030f2
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-11-02 22:36:43 -0300

    Styles finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,50 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import styles from './App.module.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
+    <main>
+      <div className={styles.container}>
+        <div className={styles.avatar}>
+          <span>
+            <img src="images/build.svg" alt="Selo Rocketseat" />
+          </span>
+          <img src="https://github.com/rodrigosaantos.png" alt="Rodrigo Santos" />
+        </div>
+        <h1 id='userName'>Rodrigo Santos</h1>
+        <a id="userLink" href="https://github.com/rodrigosaantos" target="_blank">
+          <img id="userImage" src="/images/github.svg" alt="RodrigoSaantos" />
+          <span id="userLogin">RodrigoSaantos</span>
+        </a>
+
+        <p id="userBio">
+          An instructor focused on helping people start programming for web
         </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+
+        <ul id="socialLinks">
+          <li className="youtube">
+            <a href="https://youtube.com" target="_blank">
+              <img src="images/youtube.svg" alt="Youtube" />
+            </a>
+          </li>
+          <li className="instagram">
+            <a href="https://instagram.com" target="_blank">
+              <img src="images/instagram.svg" alt="instagram" />
+            </a>
+          </li>
+          <li className="facebook">
+            <a href="https://facebook.com" target="_blank">
+              <img src="images/facebook.svg" alt="facebook" />
+            </a>
+          </li>
+          <li className="twitter">
+            <a href="https://twitter.com" target="_blank">
+              <img src="images/twitter.svg" alt="twitter" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </main>
   )
 }
 
